39652f7b4d21277d34f3d2306ef95c6a72fb316c
eFrane <efrane@meanderingsoul.com>
2019-11-10 14:39:57 +0100

    Remove globalUiComponents entry
    This entry is not necessary because this plugin does not provide
    a global UI component.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const path = require('path')
 
 module.exports = (options, ctx) => {
   return {
-    enhanceAppFiles: path.resolve(__dirname, 'mermaid.js'),
-    globalUIComponents: 'mermaid'
+    enhanceAppFiles: path.resolve(__dirname, 'mermaid.js')
   }
 }
