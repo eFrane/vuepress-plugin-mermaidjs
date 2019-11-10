5ba8c0d0338888d409e99be4065e22441dfd0783
eFrane <efrane@meanderingsoul.com>
2019-11-10 14:41:10 +0100

    Inline mermaid component
    As this component has no template it's not really necessary
    to keep it in an extra .vue file.

--- mermaid.js
+++ mermaid.js
@@ -1,4 +1,50 @@
-import Mermaid from './Mermaid.vue'
+// Since this plugin can ben used by multiple times, we need to
+// give each generated files a uid or the previous file would be
+// overwritten.
+let graphIdCounter = 0
+
+const Mermaid = {
+    name: 'Mermaid',
+    data() {
+        let graph = ''
+        if (this.$slots.default) {
+            graph = this.$slots.default[0].text
+        }
+
+        return {
+            svg: undefined,
+            graph
+        }
+    },
+    render(h) {
+        return h('div', { domProps: { innerHTML: this.svg }})
+    },
+    methods: {
+        renderSVG () {
+            import('mermaid').then(mermaid => {
+                mermaid.initialize({ startOnLoad: true })
+
+                let renderDiv = document.createElement('div')
+                document.body.appendChild(renderDiv)
+
+                mermaid.render(
+                    'mermaid' + ++graphIdCounter,
+                    this.graph,
+                    (svg) => {
+                        this.svg = svg
+                        document.body.removeChild(renderDiv)
+                    },
+                    renderDiv
+                )
+            })
+        }
+    },
+    mounted () {
+        if (0 < this.graph.length) {
+            this.renderSVG()
+        }
+    }
+}
 
 export default ({ Vue }) => {
     Vue.component('mermaid', Mermaid)
